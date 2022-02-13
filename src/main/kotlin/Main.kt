ecc85d0c137c3b1710b6cadd98a2e7729ab68054
wanjira-whitney <wanjirawhitney@gmail.com>
2022-02-13 14:16:29 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var  fullName = ("Kimberly Muthoni")
+    println(fullName)
+    var age = 18
+    println(age)
+    var phoneNumber = "0703370627"
+    println(phoneNumber)
+    var weight = 58.23
+    println(weight)
+    var citizen= "Kenyan"
+    println(citizen)
+
+
+}
